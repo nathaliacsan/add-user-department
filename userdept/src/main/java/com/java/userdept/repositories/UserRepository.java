eec476eb0065de9a463d2feb906f47d93b3eb7eb
Nathalia Santos <nathaliacsan@gmail.com>
2023-07-02 17:16:22 -0300

    Finalizando projeto

---
+++ userdept/src/main/java/com/java/userdept/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.java.userdept.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.java.userdept.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	
+}
